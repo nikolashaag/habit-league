b711ef715b593bb235cf4a08930d945c64c7a1f9
= <=>
2019-09-15 00:37:06 +0200

    fix: show description instead of hardcoded name

--- src/components/PublicChallenge.vue
+++ src/components/PublicChallenge.vue
@@ -1,39 +1,35 @@
 <template>
-    <q-card class="challenge text-white" @click="() => expanded = !expanded">
-      <q-card-section>
-        <div class="icon-wrapper">
-          <q-icon :name="getIconName(options.icon)" class="category-icon">
-          </q-icon>
-        </div>
-        <div class="header">
-          <div class="text-h6">{{options.title}}</div>
-          <div class="text-subtitle2">by John Doe</div>
-        </div>
-      </q-card-section>
+  <q-card class="challenge text-white" @click="() => expanded = !expanded">
+    <q-card-section>
+      <div class="icon-wrapper">
+        <q-icon :name="getIconName(options.icon)" class="category-icon"></q-icon>
+      </div>
+      <div class="header">
+        <div class="text-h6">{{options.title}}</div>
+        <div class="text-subtitle2">{{options.description}}</div>
+      </div>
+    </q-card-section>
 
-      <q-card-section class="justify-end row">
-         <q-btn color="amber" label="Join Challenge" @click="e => joinChallenge(e)"/>
-      </q-card-section>
+    <q-card-section class="justify-end row">
+      <q-btn color="amber" label="Join Challenge" @click="e => joinChallenge(e)" />
+    </q-card-section>
 
-      <!-- <q-separator dark /> -->
+    <!-- <q-separator dark /> -->
 
-      <q-card-actions>
-
-      </q-card-actions>
-      <q-linear-progress rounded stripe style="height: 10px" color="warning" :value="progress" />
-      <q-dialog v-model="noteProgress">
-        <q-card>
-          <q-card-section>
-            <div class="text-h6">Are you sure you want to join this challenge?</div>
-          </q-card-section>
-          <q-card-actions align="center">
-            <q-btn label="Confirm" color="primary" @click="confirm()" v-close-popup />
-            <q-btn label="Cancel" color="primary" @click="cancel()" v-close-popup />
-          </q-card-actions>
-        </q-card>
-      </q-dialog>
-
-    </q-card>
+    <q-card-actions></q-card-actions>
+    <q-linear-progress rounded stripe style="height: 10px" color="warning" :value="progress" />
+    <q-dialog v-model="noteProgress">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Are you sure you want to join this challenge?</div>
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn label="Confirm" color="primary" @click="confirm()" v-close-popup />
+          <q-btn label="Cancel" color="primary" @click="cancel()" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </q-card>
 </template>
 
 <style>
@@ -44,7 +40,7 @@ import { ICON_MAP } from '../helpers/constants'
 
 export default {
   name: 'Challenge',
-  data () {
+  data() {
     return {
       noteProgress: false,
       expanded: false
@@ -53,32 +49,35 @@ export default {
   props: ['options'],
   computed: {
     progress: {
-      get () {
+      get() {
         let dt1 = new Date(this.options.startDate)
         let dt2 = new Date()
-        let diff = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) / (1000 * 60 * 60 * 24))
+        let diff = Math.floor(
+          (Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) -
+            Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /
+            (1000 * 60 * 60 * 24)
+        )
         // If diff is 0, set to 1 (In case the challenge started today)
         diff = diff || 1
-        return (diff / (this.options.duration / 100)) / 100
+        return diff / (this.options.duration / 100) / 100
       }
     }
   },
   methods: {
-    getIconName: function (value) {
+    getIconName: function(value) {
       return ICON_MAP[value]
     },
-    joinChallenge: function (e) {
+    joinChallenge: function(e) {
       this.noteProgress = true
       e.preventDefault()
       e.stopPropagation()
     },
-    confirm: function () {
+    confirm: function() {
       this.$store.dispatch('app/joinChallenge', this.options.id)
     },
-    cancel: function () {}
+    cancel: function() {}
   },
-  created: function () {
-  }
+  created: function() {}
 }
 </script>
 
@@ -135,7 +134,8 @@ export default {
   margin-left: 10rem;
 }
 
-.week-wrapper, .leftover-wrapper {
+.week-wrapper,
+.leftover-wrapper {
   width: 100%;
 }
 
@@ -156,10 +156,12 @@ export default {
 }
 
 .icon-wrapper i {
-  position:absolute; /*it can be fixed too*/
-  left:0; right:0;
-  top:0; bottom:0;
-  margin:auto;
+  position: absolute; /*it can be fixed too*/
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
 }
 
 .header {
