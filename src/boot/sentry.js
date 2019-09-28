711eb3cbbeed54555104c2ebb69528553a45924a
Nikolas Haag <nikolas.haag@c02z1061lvdq.fritz.box>
2019-09-28 16:24:59 +0200

    fix: Show sentry dialog only on prod

--- src/boot/sentry.js
+++ src/boot/sentry.js
@@ -5,11 +5,12 @@ import * as Integrations from '@sentry/integrations'
 Sentry.init({
   dsn: 'https://7cb0bdb0c734418ebb3149d570b2514d@sentry.io/1764000',
   integrations: [ new Integrations.Vue({ Vue, attachProps: true }) ],
-  beforeSend(event, hint) {
+  ...(process.env.NODE_ENV !== 'development' &&
+  { beforeSend(event, hint) {
     // Check if it is an exception, and if so, show the report dialog
     if (event.exception) {
       Sentry.showReportDialog({ eventId: event.event_id })
     }
     return event
-  }
+  } })
 })
