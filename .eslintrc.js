9123ba5f5e029d97647f41a3a34d6a50d72cefb1
Nikolas Haag <nikolas.haag@c02z1061lvdq.fritz.box>
2019-09-28 19:44:17 +0200

    fix: Remove no console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,7 +47,7 @@ module.exports = {
     'space-before-function-paren': 0,
 
     // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
