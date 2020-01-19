154c2ac3188c01383cf3c49a51a11aa400faa761
Nikolas Haag <nikolas.haag@share-now.com>
2020-01-19 20:32:42 +0100

    feat: Sort browse challenges

--- src/pages/Browse.vue
+++ src/pages/Browse.vue
@@ -1,21 +1,38 @@
 <template>
   <q-page class="flex flex-center text-white">
-    <note v-if="showTooltip" class="text-dark" title="HOW TO USE" :onClose="onTipClose">
-      <p>Browse what habits are other people developing. by joining a habit, you make it a challenge and can compete with them.</p>
+    <note
+      v-if="showTooltip"
+      class="text-dark"
+      title="HOW TO USE"
+      :onClose="onTipClose"
+    >
+      <p>
+        Browse what habits are other people developing. by joining a habit, you
+        make it a challenge and can compete with them.
+      </p>
     </note>
     <h6>Improve your habits with your friends</h6>
     <div class="q-pa-md wrapper">
-      <public-challenge v-for="(challenge, key) in challenges" :options="challenge" :key="key" />
+      <div class="month" v-for="(month, key) in challengesByMonth" :key="key">
+        <div class="title-wrapper" >
+          <h5>{{month.month}}</h5>
+        </div>
+        <public-challenge
+          v-for="(challenge, key) in month.challenges"
+          :options="challenge"
+          :key="key"
+        />
+      </div>
     </div>
   </q-page>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 import PublicChallenge from 'components/PublicChallenge.vue'
 import Note from 'components/Note.vue'
+import moment from 'moment-timezone'
 
 export default {
   name: 'Browse',
@@ -50,11 +67,38 @@ export default {
           // If diff is 0, set to 1 (In case the challenge started today)
           diff = diff || 1
           const progress = diff / (challenge.duration / 100) / 100
-          return progress <= 0.3
+          return progress <= 0.6
         })
       },
       set(val) {
         // this.$store.commit('showcase/updateDrawerState', val)
+      }
+    },
+    challengesByMonth: {
+      get() {
+        let sorted = []
+        this.challenges.forEach(challenge => {
+          const month = moment(challenge.startDate).format('MMMM')
+          let alreadyAdded = false
+          sorted = sorted.map(sortedMonth => {
+            if (sortedMonth.month === month) {
+              alreadyAdded = true
+              return {
+                ...sortedMonth,
+                challenges: [...sortedMonth.challenges, challenge]
+              }
+            }
+            return sortedMonth
+          })
+          if (!alreadyAdded) {
+            sorted.push({
+              month: month,
+              monthNum: new Date(challenge.startDate).getMonth(),
+              challenges: [challenge]
+            })
+          }
+        })
+        return sorted.sort((a, b) => a.monthNum < b.monthNum)
       }
     }
   },
